76b00507ba3e19a761816bf907e9a2f234a6002f
adam <55722970+adampentz@users.noreply.github.com>
2019-09-28 13:37:42 -0700

    Update HelloWorld.java
    added comments header and footer

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,15 @@
+//HEADER
+//Program Name: HelloWorld
+//Author: Adam Pentz
+//Class: CS260 Fall 2019
+//Date: 09/28/2019
+//Description: This is a simple program to print a string type to console 
 
 public class HelloWorld {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
 	}
-
 }
+/*FOOTER
+Output: Hello, World!
+*/
